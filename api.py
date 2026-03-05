cd2d94b7312fa25832735781016e41c961b6b24f
Parth7234 <parthsingla2005@gmail.com>
2026-03-05 22:22:00 +0530

    Integrate Redis message broker and background worker
    This commit transitions the application into a distributed system:
    
    - update api.py to push task IDs into a Redis Sorted Set upon creation.
    - Create worker.py to continuously poll the Redis queue for high-priority tasks.
    - Worker now updates PostgreSQL state transitions (PENDING -> RUNNING -> COMPLETED).
    - Add redis to requirements.txt

--- api.py
+++ api.py
@@ -1,41 +1,40 @@
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-
-# Import new database configuration and models
+import redis  
 import database
 import models
 
-# tell SQLAlchemy to create the tables in PostgreSQL if they don't exist yet
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="Distributed Task Scheduler API")
 
-# Define the exact data structure expected from the user
+# connect to the local Redis server 
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
 class TaskRequest(BaseModel):
     priority: int
     description: str
 
-# Endpoint 1: Add a task to the database
 @app.post("/tasks")
 def create_task(request: TaskRequest, db: Session = Depends(database.get_db)):
-    # 1. Create a new Python object representing the row
+    # save task to the PostgreSQL database
     new_task = models.Task(
         priority=request.priority,
         description=request.description,
         status="PENDING"
     )
     
-    # 2. Add it to the session and commit it to the database
     db.add(new_task)
     db.commit()
-    db.refresh(new_task) # This grabs the auto-generated ID from PostgreSQL
+    db.refresh(new_task) # grab new ID from the database
     
-    return {"message": "Task added successfully!", "task": new_task}
+    # add Task ID to Redis priority queue  
+    redis_client.zadd("task_queue", {str(new_task.id): new_task.priority})
+    
+    return {"message": "Task added to database and queued in Redis!", "task": new_task}
 
-# Endpoint 2: View the current queue from the database
 @app.get("/tasks")
 def view_queue(db: Session = Depends(database.get_db)):
-    # 3. Query the database for all tasks, ordered by priority (lowest number first)
     tasks = db.query(models.Task).order_by(models.Task.priority.asc()).all()
     return {"current_queue_size": len(tasks), "tasks": tasks}
---
+++ worker.py
@@ -0,0 +1,59 @@
+import time
+import redis
+from database import SessionLocal
+import models
+
+# connect to Redis
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+def start_worker():
+    print("Worker started. Fetching tasks from the Redis queue...")
+    
+    # infinite loop to keep the worker alive 24/7
+    while True:
+        # ask Redis for the highest priority task (lowest score)
+        task_data = redis_client.zpopmin("task_queue", count=1)# zpopmin pops the item with the lowest score off the sorted set
+        
+        if not task_data:
+            # If the queue is empty, rest for 1 second 
+            time.sleep(1)
+            continue
+            
+        # unpack the data given by Redis
+        # task_data format: [('1', 2.0)] -> [('task_id', priority_score)]
+        task_id_str, priority = task_data[0]
+        task_id = int(task_id_str)
+        
+        print(f"\n[Worker] Grabbed Task ID: {task_id} (Priority: {priority})")
+        
+        # open a database session to look up what the task is
+        db = SessionLocal()
+        try:
+            task = db.query(models.Task).filter(models.Task.id == task_id).first()
+            
+            if task:
+                # mark task as RUNNING so the API knows we are working on it
+                task.status = "RUNNING"
+                db.commit()
+                
+                print(f"[Worker] Executing: '{task.description}'...")
+                
+                # heavy work simulation (e.g., training a model, sending emails)
+                time.sleep(5) 
+                
+                # mark task as completed
+                task.status = "COMPLETED"
+                db.commit()
+                print(f"[Worker] Successfully finished Task ID: {task_id} ")
+            else:
+                print(f"[Worker] Error: Task {task_id} not found in database!")
+
+        except Exception as e: #catch block
+            print(f"[Worker] Error while executing task {task_id}: {e}")
+
+        finally: #finally block always executes no matter success or failure
+            # close the connection when done 
+            db.close()
+
+if __name__ == "__main__":
+    start_worker()
